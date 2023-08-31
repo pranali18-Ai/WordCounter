636ce3264d98abda6018f1491959f9f376d46789
Pranali Patil <patilpranali0018@gmail.com>
2023-08-31 12:32:28 +0530

    31(1) Aug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,9 @@ export default function Navbar(props) {
               <Link className="nav-link active" to="/about">About</Link>
             </li>
           </ul>
-
+<div className='d-flex'>
+  <div className='bg-primary rounded mx-2' style={{height: '30px', width:'30px'}}></div>
+</div>
           <form className="d-flex mx-2">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
